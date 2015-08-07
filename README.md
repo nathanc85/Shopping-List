062ec7746cf4eef2b415a4b258c66974e0de7fb5
Nathan Costea <nathanC@knowclassic.com>
2015-08-07 13:26:33 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shopping-List
+A simple Shopping List. Meant to test some things out for now.
---
+++ shopping.js
@@ -0,0 +1,46 @@
+ShoppingList = new Mongo.Collection('shoppingList');
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    shoppingList: function() {
+      return ShoppingList.find();
+    }
+  });
+
+  Template.body.events({
+    'submit .newShoppingList' : function(event) {
+      //var item = event.target.newItem.value;
+      var item = $('input[name="newItem"]').val();
+      ShoppingList.insert({
+        item: item,
+        createdDate: new Date()
+      });
+
+      //event.target.item.value = "";
+      $('.newShoppingList')[0].reset();
+
+      return false;
+    },
+    'change .hideShow' : function(event) {
+      Session.set('hideShow', event.target.checked);
+    }
+  });
+
+  Template.body.helpers({
+    'showSessions' : function() {
+      return Session.get('hideShow');
+    }
+  });
+
+  Template.line.events({
+    'click .remove': function() {
+      ShoppingList.remove(this._id);
+    },
+    'change .hideShow': function() {
+      ShoppingList.update(this._id, {$set: {
+        checked: !this.checked
+      }});
+    }
+  });
+
+}
