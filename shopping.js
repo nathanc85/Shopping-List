39cc0156e9c7525eae3eceabb12f14f65dfbb93d
Nathan Costea <nathanC@knowclassic.com>
2015-08-21 09:08:06 -0400

    Introduced the privacy concept per user Removed autopublish Created pubsub functionality Updated Meteor

--- shopping.js
+++ shopping.js
@@ -1,6 +1,8 @@
 ShoppingList = new Mongo.Collection('shoppingList');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('shoppingList');
+
   Template.body.helpers({
     shoppingList: function() {
       if (Session.get('hideShow')) {
@@ -26,6 +28,7 @@ if (Meteor.isClient) {
     },
     'change .hideShow' : function(event) {
       Session.set('hideShow', event.target.checked);
+      //$(".col-md-3.text-center").html('Hello');
     }
   });
 
@@ -35,24 +38,60 @@ if (Meteor.isClient) {
     },
     'change .completed': function() {
       Meteor.call('updateItem', this._id, !this.checked);
+    },
+    'click :button[name=privacy]' : function() {
+      Meteor.call('updatePrivacy', this._id, !this.privacy);
+    }
+  });
+
+  Template.line.helpers({
+    'isOwner': function() {
+      return (this.owner === Meteor.userId());
     }
   });
 
 }
 
+
+if (Meteor.isServer) {
+  Meteor.publish('shoppingList', function () {
+    return ShoppingList.find(
+      {$or: [
+        {privacy: false},
+        {owner: this.userId}
+      ]}
+    );
+  });
+}
+
+
 Meteor.methods({
   addItem : function (item) {
     ShoppingList.insert({
+      owner : Meteor.userId(),
+      privacy : false,
+      checked: false,
       item: item,
       createdDate: new Date()
     });
   },
   updateItem : function(id, checked) {
-    ShoppingList.update({_id: id},{$set: {
+    ShoppingList.update({_id: id}, {$set: {
       checked: checked
     }});
   },
-  removeItem : function (id) {
+  removeItem : function(id) {
     ShoppingList.remove(id);
+  },
+  updatePrivacy : function(id, privacy) {
+    ShoppingList.update({_id: id}, {$set: {
+      privacy: privacy
+    }});
   }
 });
+
+//Template.registerHelper('isOwner',
+//  function(v1, v2) {
+//    return (v1 === v2);
+//  }
+//);
